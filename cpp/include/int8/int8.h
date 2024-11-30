f99b2b9b1328687aa64a23fe42f8dd90cebd8c95
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2024-11-30 11:42:16 -0600

    Adding Int8

---
+++ cpp/include/int8/int8.h
@@ -0,0 +1,28 @@
+#ifndef INT_8
+#define INT_8
+
+#include "../quant/quant.h"
+#include <string>
+
+const int INT8_NUM_INT_BITS = 3;
+const int INT8_NUM_DEC_BITS = 4;
+const int MAX_INT8 = 0x7F;
+
+class Int8: Dtype{
+private:
+    int8_t _num;
+public:
+    Int8(float x);
+    Int8 operator+(Int8&& other);
+    Int8 operator+(Int8& other);
+    Int8 operator*(Int8&& other);
+    Int8 operator*(Int8& other);
+    std::string str();
+    void print();
+};
+
+typedef std::vector<Int8> int8_vec;
+
+void print(Int8&& num);
+
+#endif
