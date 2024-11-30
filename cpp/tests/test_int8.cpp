fcd366fc6eae5e7b8068d99b8aad43e2415b60e2
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2024-11-30 14:46:07 -0600

    Adding more functions to int8 test

--- cpp/tests/test_int8.cpp
+++ cpp/tests/test_int8.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #include "../include/int8/int8.h"
+#include "../include/nn/nn.h"
 
 using namespace std;
 
 int main(int argc, char **argv){
-    if(argc != 3)
+    if(argc < 3)
         return 0;
 
-    Int8 a(stof(argv[1]));
-    Int8 b(stof(argv[2]));
+    int8_vec vec;
+    for(int i = 1; i < argc; ++i)
+        vec.push_back(stof(argv[i]));
 
-    Int8 sum = a + b;
-    sum.print();
-
-    Int8 mult = a * b;
-    mult.print();
+    int len = argc -1;
+    for(int i = 1; i < len; ++i){
+        Int8 a(vec[i - 1]);
+        Int8 b(vec[i]);
+        Int8 sum = a + b;
+        Int8 mult = a * b;
+        Int8 relu_a = ReLU(a);
+        Int8 relu_b = ReLU(b);
+        cout << "-----------" << endl;
+        a.print();
+        b.print();
+        sum.print();
+        mult.print();
+        relu_a.print();
+        relu_b.print();
+    }
 
     return 0;
 }
