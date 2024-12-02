ad1c045c26b8d9a1cd137efa720519ed4bde2bce
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2024-12-01 19:45:01 -0600

    Adding quantization to nn for testing. This cant be merged into master

--- cpp/include/nn/nn.cpp
+++ cpp/include/nn/nn.cpp
@@ -1,4 +1,5 @@
 #include "nn.h"
+#include "../wquant/wquant.h"
 #include <iostream>
 #include <stdexcept>
 #include <fstream>
@@ -73,6 +74,12 @@ std::vector<std::string> get_model_files(std::string& fpath){
 }
 
 Perceptron::Perceptron(std::string& fpath): _depth(0){
+    std::vector<Quantizer> qs{
+        Quantizer(8, -0.21828389167785644531, 0.22750344872474670410),
+        Quantizer(8, -0.28965741395950317383, 0.37081623077392578125),
+        Quantizer(8, -0.25820058584213256836, 0.42827829718589782715),
+        Quantizer(8, -0.28323006629943847656, 0.25655370950698852539)
+    };
     std::cout << "Loading weights for inferecne." << std::endl;
     std::vector<std::vector<std::vector<float>>> weights;
     
@@ -81,7 +88,9 @@ Perceptron::Perceptron(std::string& fpath): _depth(0){
     for(int i = 0; i < nlayers; ++i){
         std::cout << "Loading layer " << i+1 << "/" << nlayers << std::endl;
         std::vector<std::vector<float>> layer_ws = read_weights_vector(model_files[i]);
-        _layers.push_back(Layer(layer_ws));
+        std::vector<std::vector<int>> quantized = qs[i].quantize(layer_ws);
+        std::vector<std::vector<float>> dequantized = qs[i].dequantize(quantized);
+        _layers.push_back(Layer(dequantized));
         _depth++;
     }
     _layers[nlayers - 1].as_output_layer();
