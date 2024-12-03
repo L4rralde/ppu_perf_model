1ba6dc0b6c99857df0bff0e6b2dc205b93425af0
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2024-12-03 10:35:47 -0600

    check quantiztion is working and printing more stuff

--- cpp/src/quantize_weights.cpp
+++ cpp/src/quantize_weights.cpp
@@ -37,27 +37,42 @@ int main(){
         throw runtime_error("$GIT_ROOT must be defined first");
     string s(tmp);
     string files_path = s + "/cpp/model/";
-    string qmodel_path = s + "/cpp/qmodel/model.bin";
+    string qmodel_path = s + "/cpp/qmodel/";
     vector<string> model_files = get_model_files(files_path);
     int num_files = model_files.size();
 
-    vector<int8_t> all_ws;
     for(int i = 0; i < num_files; ++i){
         string src_path(model_files[i]);
         cout << "Starting conversion of " << src_path << endl;
         vector< vector<float> > ws = read_weights_vector(src_path);
-        vector<float> flat = flatten(ws);
-        Quantizer quanto(8, flat);
-        vector<int> quantized = quanto.quantize(flat);
-        int len = quantized.size();
-        vector<int8_t> qbytes(len, 0.0);
-        for(int i = 0; i < len; ++i)
-            qbytes[i] = static_cast<int8_t>(quantized[i]);
-        all_ws.insert(all_ws.end(), qbytes.begin(), qbytes.end());
-        continue;
-    }
-    binfile::write<int8_t>(all_ws, qmodel_path);
+        unsigned long rows = ws.size();
+        unsigned long cols = ws[0].size();
+        Quantizer quanto(8, ws);
+        quanto.print();
+        cout << rows << ", " << cols << endl;
+        vector< vector<int> > quantized_ws = quanto.quantize(ws);
+        vector<int> flat = flatten(quantized_ws);
+        int len = flat.size();
+        vector<int8_t> qbytes(flat.begin(), flat.end());
+        string binfpath = qmodel_path + to_string(i) + ".txt";
+        binfile::write<int8_t>(qbytes, binfpath);
+        //Check
+        vector<int8_t> read_qbytes = binfile::read<int8_t>(binfpath);
+        vector<int> read_quantized(read_qbytes.begin(), read_qbytes.end());
+        vector<vector<int>> read_rehasped = reshape(
+            read_quantized, rows, cols
+        );
+        for(unsigned long i = 0; i < rows; ++i)
+            for(unsigned long j = 0; j < cols; ++j)
+                if(read_rehasped[i][j] != quantized_ws[i][j])
+                    throw runtime_error("Mismatch.");
 
-    cout << "Correctly quantized model" << endl;
+        vector<vector<float>> read_ws = quanto.dequantize(read_rehasped);
+        cout << "Error: " << mse(ws, read_ws) << endl;
+    }
+
+    cout << "Correctly quantized model."  << endl;
+    cout << "PASS" << endl;
+
     return 0;
 }
